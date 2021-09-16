30f4935a3a4de7578d561dbadedd331e0f7fdabc
Adil <mamyrkhanov.adil@gmail.com>
2021-09-17 05:18:39 +0600

    Add EM with async DB querying

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'mysql2', '~> 0.5.3'
 gem 'dotenv', '~> 2.7', '>= 2.7.6'
+gem 'eventmachine'
+gem 'pry'
--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
-require 'mysql2'
+require 'mysql2/em'
 require 'dotenv/load'
+require 'eventmachine'
+require 'pry'
 
 class Correction
     def initialize(val)
@@ -66,6 +68,29 @@ class Correction
     end
 end
 
+def partition(size)
+    case size
+    when 1..500
+        1
+    when 501..1000
+        2
+    when 1001..1500
+        3
+    when 1501..2000
+        4
+    when 2001..2500
+        4
+    when 2501..3000
+        5
+    when 3001..3500
+        6
+    when 3500..4000
+        7
+    else
+        0
+    end
+end
+
 update_query = ""
 
 # Needs .env file
@@ -76,35 +101,59 @@ client = Mysql2::Client.new(
     password: ENV['PASSWORD'], 
 )
 client.query("delete from hle_dev_test_adil_mamyrkhanov where candidate_office_name = '';")
+count_result = client.query("select count(id) from hle_dev_test_adil_mamyrkhanov a;")
+table_count = count_result.first['count(id)']
+delim = partition(table_count)
+limit = (table_count / delim).ceil
 table = client.query("select id, candidate_office_name from hle_dev_test_adil_mamyrkhanov a;", :cast => false)
+EM.run do
+    table.each_with_index do |row, i|
+        id = row['id']
+        name = row['candidate_office_name']
 
-table.each_with_index do |row, i|
-    id = row['id']
-    name = row['candidate_office_name']
+        clean_name = Correction.new(name).perform
+        sentence = "The candidate is running for the #{clean_name} office"
+        update_query += "update hle_dev_test_adil_mamyrkhanov set clean_name=\"%s\", sentence=\"%s\" where id = %s;\n" % [clean_name, sentence, id.to_s]
 
-    clean_name = Correction.new(name).perform
-    sentence = "The candidate is running for the #{clean_name} office"
-    update_query += "update hle_dev_test_adil_mamyrkhanov set clean_name=\"%s\", sentence=\"%s\" where id = %s;\n" % [clean_name, sentence, id.to_s]
+        # update db with a part of all update statements. The statements splited according to total / partition
+        if i % limit > limit - 2
+            updating_client = Mysql2::EM::Client.new(
+                host: ENV['HOST'], 
+                database: ENV['DATABASE'], 
+                username: ENV['USERNAME'], 
+                password: ENV['PASSWORD'], 
+                flags: Mysql2::Client::MULTI_STATEMENTS
+            )
+            defer = updating_client.query(update_query, :async => true)
+            defer.callback do |result|
+                puts "Result of #{i} statements: #{result.to_a.inspect}"
+                updating_client.close
+            end
+            update_query = ''
+        end
+    end
+
+
+    unless update_query.empty?
+        updating_client = Mysql2::EM::Client.new(
+            host: ENV['HOST'], 
+            database: ENV['DATABASE'], 
+            username: ENV['USERNAME'], 
+            password: ENV['PASSWORD'], 
+            flags: Mysql2::Client::MULTI_STATEMENTS
+        )
+        defer = updating_client.query(update_query, :async => true)
+        defer.callback do |result|
+            puts "Last statements: #{result.to_a.inspect}"
+            updating_client.close
+        end
+        update_query = ''
+        EventMachine::stop_event_loop
+    else
+        EventMachine::stop_event_loop
+    end
 end
-
-updating_client = Mysql2::Client.new(
-    host: ENV['HOST'], 
-    database: ENV['DATABASE'], 
-    username: ENV['USERNAME'], 
-    password: ENV['PASSWORD'], 
-    flags: Mysql2::Client::MULTI_STATEMENTS
-)
-print 'Update statements sent to MySQL, wait...'
-updating_client.query(update_query)
-while updating_client.next_result
-  @result = updating_client.store_result
-  sleep 3
-  print '.'
-end
-puts @result
 puts "Done!"
-updating_client.close
-update_query = ''
 
 
 
