17b180a7501dfbb6fdff8afd499b957b796771f3
Adil <mamyrkhanov.adil@gmail.com>
2021-09-16 18:41:54 +0600

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'mysql2', '~> 0.5.3'
+gem 'dotenv', '~> 2.7', '>= 2.7.6'
---
+++ app.rb
@@ -0,0 +1,98 @@
+require 'mysql2'
+require 'dotenv/load'
+
+class Correction
+    def initialize(val)
+        @name = val
+        @parts = []
+    end
+
+    def perform
+        change_abbrs
+        delete_dot
+        split_by_slash
+        add_parentheses
+        rotate_by_array_length
+        normalize
+        @name
+    end
+
+    private
+
+    def change_abbrs
+        abbrs = {
+            "Twp" => "Township",
+            "Hwy" => "Highway",
+            "U" => "You",
+            "CH" => "Chapter"
+        }
+        abbrs.each { |k,v| @name.gsub!(k,v)}
+    end
+
+    def delete_dot
+        @name.delete!('.')
+    end
+
+    def split_by_slash
+        @parts = @name.split('/').map(&:strip)
+    end
+
+    def add_parentheses
+        @parts.map do |p|
+            if p.include? ?,
+                comma_parts = p.split(',').map(&:strip)
+                comma_parts.first.downcase!
+                comma_parts[comma_parts.length] = comma_parts.last.prepend('(') << ')'
+                p = comma_parts.join(' ')
+            end
+            p
+        end
+    end
+
+    def rotate_by_array_length
+       for _ in 1..@parts.length - 1 do
+           @parts = @parts.rotate(1)
+       end
+    end
+
+    def normalize
+        @parts.map { |p| p.downcase! } if @parts.length == 1
+        @parts.drop(1).map { |p| p.downcase! } if @parts.length > 1
+        @parts.insert(-2, 'and') if @parts.length > 2
+        @name = @parts.join(' ')
+        max = @name.scan(/\S+/).length
+        1.upto(max).each_with_object(@name) do |n, s| 
+            s.gsub!(/((?:\b\s*\S+){#{n}})\1/i, '\1')
+        end
+    end
+end
+
+insert_query = ""
+
+# Needs .env file
+client = Mysql2::Client.new(
+    host: ENV['HOST'], 
+    database: ENV['DATABASE'], 
+    username: ENV['USERNAME'], 
+    password: ENV['PASSWORD'], 
+)
+client.query("delete from hle_dev_test_adil_mamyrkhanov where candidate_office_name = '';")
+table = client.query("select id, candidate_office_name from hle_dev_test_adil_mamyrkhanov a;")
+table.each do |row|
+    id = row['id']
+    name = row['candidate_office_name']
+
+    clean_name = Correction.new(name).perform
+    sentence = "The candidate is running for the #{clean_name} office"
+    insert_query += "update hle_dev_test_adil_mamyrkhanov set clean_name=\'%s\', sentence=\'%s\' where id = %s;\n" % [clean_name, sentence, id.to_s]
+end
+
+
+updating_client = Mysql2::Client.new(
+    host: ENV['HOST'], 
+    database: ENV['DATABASE'], 
+    username: ENV['USERNAME'], 
+    password: ENV['PASSWORD'], 
+    flags: Mysql2::Client::MULTI_STATEMENTS
+)
+updating_client.query(insert_query)
